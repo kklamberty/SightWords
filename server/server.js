237eb1af583da86616b8f3a4f0aa20adbf9f98af
kklamberty <lamberty@morris.umn.edu>
2014-08-17 19:36:38 -0500

    Trying to remove the node_modules folder and its contents

--- server/server.js
+++ server/server.js
@@ -1,3 +1,23 @@
 var mongoose = require("mongoose");
-//mongoose.connect('mongodb://localhost/sightwords');
+var Wordlist = require("../schemas/Wordlist");
+var config = require('../config');
+
+mongoose.connect(config.db.development);
+
+
+
+
+//
+//Manually adding things to the dev database
+//
+
+//var wordlist = new Wordlist({name: "thirdList", words:["pear", "peach", "mango"], includeInCurrent: false});
+//wordlist.save(function (err) {
+//    if(err) {
+//        console.log("failed");
+//    } else {
+//        console.log("saved");
+//    }
+//})
+
 
